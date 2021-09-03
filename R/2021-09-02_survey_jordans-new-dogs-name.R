f73fbf471182330b9d37e4a65667f0b0d1353621
Jordan <52896238+Jordan-Krogmann@users.noreply.github.com>
2021-09-03 16:43:09 -0400

    added gif

--- R/2021-09-02_survey_jordans-new-dogs-name.R
+++ R/2021-09-02_survey_jordans-new-dogs-name.R
@@ -2,6 +2,7 @@
 # load packages -----
 library(googlesheets4)
 library(tidyverse)
+library(lubridate)
 library(gganimate)
 
 
@@ -10,7 +11,7 @@ df <- read_sheet(
   "https://docs.google.com/spreadsheets/d/1l3dfi1_8yHRYTYZODdDH5Eu7T0c5S4zwsU27_eTVW-A/edit?resourcekey#gid=1833286313"
 ) %>% 
   janitor::clean_names() %>% 
-  mutate(timestamp = lubridate::as_datetime(timestamp))
+  mutate(timestamp = round_date(as_datetime(timestamp), "5 mins"))
 
 # eda -----
 # create static plot
@@ -38,10 +39,7 @@ p
 
 # save plot 
 ggsave(
-  filename = here::here(
-    "images",
-    "2021-09-02_jordans-pet.png"
-  ),
+  filename = here::here("images","2021-09-02_jordans-pet.png"),
   plot = p,
   width = 10,
   height = 5
@@ -49,28 +47,39 @@ ggsave(
 
 
 # create gif
-df %>% 
-  filter(clean_email == 1) %>% 
-  group_by(names, timestamp) %>%
-  summarise(n = n()) %>%
+g <- 
+  df %>% 
+  filter(clean_email == 1) %>%
+  group_by(names, timestamp) %>% 
+  summarise(n = n()) %>% 
+  mutate(n = cumsum(n)) %>% 
+  group_by(names) %>% 
+  mutate(time = row_number()) %>% 
+  ungroup() %>% 
   ggplot(aes(y = reorder(names, n), x = n, fill = names)) + 
   geom_col(color = "black") + 
-  geom_label(aes(label = n), fill = "white") +
   theme_minimal() + 
   theme(legend.position = "none") + 
-  gganimate::transition_reveal(timestamp)
+  geom_label(aes(label = as.character(n)), fill = "white") + 
+  transition_reveal(time) + 
+  labs(
+    title = "Jordan's New Dog's Name",
+    subtitle = glue::glue("Out of a total of {sum(df$clean_email, na.rm = TRUE)} responses"),
+    y = NULL,
+    x = "Count (#)",
+    caption = "Data Source | Google Sheets"
+  )
 
 
+animate(
+  g,
+  fps = 30,
+  duration = 15,
+  end_pause = 5
+)
 
-
-
-
-
-
-
-
-
-
-
-
-
+gganimate::anim_save(
+  filename =  "2021-09-02_jordans-pet.gif",
+  path = here::here("images"),
+  animation = last_animation()
+)
